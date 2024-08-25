2d53b053668854e6fdec5d7ebe04f5b61c8aff9a
Saraí Rojas García <rojasg.sarai@gmail.com>
2024-08-25 17:55:29 -0600

    Add files via upload

---
+++ juju.java
@@ -0,0 +1,16 @@
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = slides.children.length;
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    const offset = -currentIndex * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
